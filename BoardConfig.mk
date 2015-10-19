e9200d6b7d9eb2907f6d67d621d9729fd57c27ba
Steve Kondik <steve@cyngn.com>
2015-10-19 14:43:39 -0700

    lavender: power-libperfmgr: Add support for tap-to-wake feature control
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Change-Id: Ideb02c659d64cdde4ee2f46c2631f7b42d003b05
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,9 +107,6 @@ USE_OPENGL_RENDERER := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
