78c4e15386f566ddbee4a4a6dbbada22cb881cd3
ImSpiDy <SpiDy2713@gmail.com>
2022-06-23 00:27:08 +0530

    lavender: bringup for nusa
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- nad_lavender.mk
+++ nad_lavender.mk
@@ -16,11 +16,10 @@ $(call inherit-product, device/xiaomi/lavender/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Inherit some common Corvus stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
-$(call inherit-product, device/corvus/CorvusParts/corvusparts.mk)
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := du_lavender
+PRODUCT_NAME := nad_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -32,7 +31,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 20.3.19 release-keys"
 
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
-
-DU_BUILD_TYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
